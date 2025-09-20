5e07ccad1593430d0e3b38570dd894de4d9c7dba
Sammi Johnson <sambjohnson94@protonmail.com>
2025-09-19 19:50:56 -0700

    add SDL3 boilerplate

--- pls-chip8/main.c
+++ pls-chip8/main.c
@@ -1,39 +1,52 @@
-#define SDL_MAIN_USE_CALLBACKS 1
+/* simple-playback-callback.c ... */
+
+/*
+ * This example code creates a simple audio stream for playing sound, and
+ * generates a sine wave sound effect for it to play as time goes on. Unlike
+ * the previous example, this uses a callback to generate sound.
+ *
+ * This might be the path of least resistance if you're moving an SDL2
+ * program's audio code to SDL3.
+ *
+ * This code is public domain. Feel free to use it for any purpose!
+ */
+
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#include <float.h>
-#include "chip8.c"
+#include <stdio.h>
 
-static const int SAMPLE_RATE = 44100;
+/* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 static SDL_AudioStream *stream = NULL;
 static int current_sine_sample = 0;
 
+/* this function will be called (usually in a background thread) when the audio stream is consuming data. */
 static void SDLCALL FeedTheAudioStreamMore(void *userdata, SDL_AudioStream *astream, int additional_amount, int total_amount)
 {
+    /* total_amount is how much data the audio stream is eating right now, additional_amount is how much more it needs
+       than what it currently has queued (which might be zero!). You can supply any amount of data here; it will take what
+       it needs and use the extra later. If you don't give it enough, it will take everything and then feed silence to the
+       hardware for the rest. Ideally, though, we always give it what it needs and no extra, so we aren't buffering more
+       than necessary. */
     additional_amount /= sizeof (float);  /* convert from bytes to samples */
     while (additional_amount > 0) {
         float samples[128];  /* this will feed 128 samples each iteration until we have enough. */
         const int total = SDL_min(additional_amount, SDL_arraysize(samples));
-        float dphase, phase;
-        int i, freq;
+        int i;
 
-        freq = 220;
-        phase = 0.00;
-        dphase = (float)freq / SAMPLE_RATE; /* 440 Hz / 44.1 kHz = 0.009977 */
-
-        /* generate a 440Hz tone */
+        /* generate a 440Hz pure tone */
         for (i = 0; i < total; i++) {
-            samples[i] = SDL_sinf(2 * SDL_PI_F * phase);
-            phase += dphase;
-            if (phase >= 1.0)
-                phase -= 1.0;
-            printf("%f\t", phase);
+            const int freq = 440;
+            const float phase = current_sine_sample * freq / 8000.0f;
+            printf("%5.2f\t", phase);
+            samples[i] = SDL_sinf(phase * 2 * SDL_PI_F);
+            current_sine_sample++;
         }
 
         /* wrapping around to avoid floating-point errors */
-        current_sine_sample %= SAMPLE_RATE;
+        current_sine_sample %= 8000;
 
         /* feed the new data to the stream. It will queue at the end, and trickle out as the hardware needs more data. */
         SDL_PutAudioStreamData(astream, samples, total * sizeof (float));
@@ -41,6 +54,7 @@ static void SDLCALL FeedTheAudioStreamMore(void *userdata, SDL_AudioStream *astr
     }
 }
 
+/* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
     SDL_AudioSpec spec;
@@ -52,44 +66,55 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
+    /* we don't _need_ a window for audio-only things but it's good policy to have one. */
     if (!SDL_CreateWindowAndRenderer("examples/audio/simple-playback-callback", 640, 480, 0, &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
+    /* We're just playing a single thing here, so we'll use the simplified option.
+       We are always going to feed audio in as mono, float32 data at 8000Hz.
+       The stream will convert it to whatever the hardware wants on the other side. */
     spec.channels = 1;
-    spec.format = SDL_AUDIO_F32LE;
-    spec.freq = SAMPLE_RATE;
+    spec.format = SDL_AUDIO_F32;
+    spec.freq = 8000;
     stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, FeedTheAudioStreamMore, NULL);
     if (!stream) {
         SDL_Log("Couldn't create audio stream: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
+    /* SDL_OpenAudioDeviceStream starts the device paused. You have to tell it to start! */
     SDL_ResumeAudioStreamDevice(stream);
 
-    return SDL_APP_CONTINUE;
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
     if (event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS;
+        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
+/* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
+    /* we're not doing anything with the renderer, so just blank it out. */
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
-    return SDL_APP_CONTINUE;
+    /* all the work of feeding the audio stream is happening in a callback in a background thread. */
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
-
+/* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
     /* SDL will clean up the window/renderer for us. */
     printf("\n");
 }
+
